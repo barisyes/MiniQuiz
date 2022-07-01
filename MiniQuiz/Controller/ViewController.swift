a21e97c4133ba18715e01a9c9ad06e0eab680a0c
barisyes <barisyesilkayaa@gmail.com>
2022-07-01 17:40:10 +0300

    first commit

---
+++ MiniQuiz/Controller/ViewController.swift
@@ -0,0 +1,55 @@
+//
+//  ViewController.swift
+//  MiniQuiz
+//
+//  Created by Barış Yeşilkaya on 1.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    var quizBrain = QuizBrain()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        starting()
+    }
+
+    @IBAction func pressedButton(_ sender: UIButton) {
+        
+        let userAnswer = sender.currentTitle!
+        let answerResult = quizBrain.checkAnswer(userAnswer)
+        
+        if answerResult{
+            sender.backgroundColor = UIColor.green
+        }else{
+            sender.backgroundColor = UIColor.red
+        }
+        
+        quizBrain.nextQuestion()
+        
+        Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(starting), userInfo: nil, repeats: false)
+
+    }
+    
+    @objc func starting(){
+        questionLabel.text = quizBrain.getQuestionText()
+        scoreLabel.text = "Score: \(quizBrain.getScore())"
+        button1.setTitle(quizBrain.getButtonName(buttonNumber: 1), for: .normal)
+        button2.setTitle(quizBrain.getButtonName(buttonNumber: 2), for: .normal)
+        button3.setTitle(quizBrain.getButtonName(buttonNumber: 3), for: .normal)
+        button1.backgroundColor = UIColor.clear
+        button2.backgroundColor = UIColor.clear
+        button3.backgroundColor = UIColor.clear
+    }
+    
+}
+
---
+++ MiniQuiz/Model/Question.swift
@@ -0,0 +1,20 @@
+//
+//  Question.swift
+//  MiniQuiz
+//
+//  Created by Barış Yeşilkaya on 1.07.2022.
+//
+
+import Foundation
+
+struct Question {
+    let text: String
+    let answer: [String]
+    let correctAnswer: String
+    
+    init(q: String, a: [String], correctAnswer: String) {
+        self.text = q
+        self.answer = a
+        self.correctAnswer = correctAnswer
+    }
+}
